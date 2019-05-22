de32a1556c07d1fc7e1262363791e29f7182e8bb
Kyle <kaplaugher94@gmail.com>
2019-05-22 16:34:36 -0500

    updates

--- src/App.vue
+++ src/App.vue
@@ -1,56 +1,22 @@
 <template>
-  <div
-    id="app"
-    class="flex bg-gray-900 text-white min-h-screen"
-  >
-    <div class="nav w-24 fixed min-h-screen text-gray-500 flex justify-center items-center">
-      <ul>
-        <li class="mb-10">
-          <a
-            href="http://"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="hover:text-white"
-          >Hi
-          </a>
-        </li>
-        <li class="mb-10">
-          <a
-            href="http://"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="hover:text-white"
-          >Hi
-          </a>
-        </li>
-        <li class="mb-10">
-          <a
-            href="http://"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="hover:text-white"
-          >Hi
-          </a>
-        </li>
-        <li class="mb-10">
-          <a
-            href="http://"
-            target="_blank"
-            rel="noopener noreferrer"
-            class="hover:text-white"
-          >Hi
-          </a>
-        </li>
-
-      </ul>
-    </div>
-    <div class="w-full ml-24">
+  <div id="app" class="flex bg-gray-900 text-white min-h-screen">
+    <SidebarNavigation />
+    <div class="w-full">
       <div class="container mx-auto">
         <router-view />
       </div>
     </div>
   </div>
 </template>
+
+<script>
+import SidebarNavigation from '@/components/SidebarNavigation.vue';
+export default {
+  components: {
+    SidebarNavigation
+  }
+};
+</script>
 
 <style src="./css/main.css">
 </style>
--- src/main.js
+++ src/main.js
@@ -1,7 +1,12 @@
-import Vue from "vue";
-import App from "./App.vue";
-import router from "./router";
-import store from "./store";
+import Vue from 'vue';
+import App from './App.vue';
+import router from './router';
+import store from './store';
+
+import VueGlide from 'vue-glide-js';
+import 'vue-glide-js/dist/vue-glide.css';
+
+Vue.use(VueGlide);
 
 Vue.config.productionTip = false;
 
@@ -9,4 +14,4 @@ new Vue({
   router,
   store,
   render: h => h(App)
-}).$mount("#app");
+}).$mount('#app');
